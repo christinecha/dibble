b2eb73bd065580b48ca81d0ecff75e54fe27f50a
CHRISTINE CHA <HELLO@CHRISTINECHA.COM>
2015-08-18 21:37:02 -0400

    First commit.

--- main.js
+++ main.js
@@ -1,9 +1,47 @@
-var TODOS = ["buy food", "make food", "eat food"];
-
-var TodoApp = React.createClass({
-    render: function(){
-        return(<div>{this.props.todos}</div>);
-    },
+var TodoList = React.createClass({
+  render: function() {
+    var createItem = function(todos, index){
+      return <li key={index}>{todos}</li>;
+    };
+    return <ul>{this.props.todos.map(createItem)}</ul>;
+  }
 });
 
-React.render(<TodoApp todos={TODOS} />, document.getElementById('content'));
+var TodoApp = React.createClass({
+  getInitialState: function(){
+    return {
+      text:"",
+      todos:[],
+      todoLists:[],
+      index: 0
+    };
+  },
+  onChange: function(e){
+    this.setState({text: e.target.value})
+  },
+  handleClick: function(){
+    this.setState(
+      {todos: this.state.todos.concat(this.state.text)}
+    );
+  },
+  newList: function(){
+    this.setState({
+      todoLists: this.state.todoLists.concat([this.state.todos]),
+      index: this.state.index += 1
+    });
+    console.log(this.state.todoLists);
+    console.log(this.state.index);
+  },
+  render: function(){
+    return (
+      <div>
+        <TodoList todos={this.state.todos} />
+        <input type="text" value={this.state.text} onChange={this.onChange} />
+        <input type="submit" onClick={this.handleClick}/>
+        <button onClick={this.newList}>New List</button>
+      </div>
+    );
+  }
+});
+
+React.render(<TodoApp />, document.getElementById('content'));
