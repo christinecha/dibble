f923a4cf0b4716110e7cd94ba7572edfbfc495d6
Christine Cha <hello@christinecha.com>
2015-10-09 20:12:33 -0400

    Basic Activity page fully functional.

--- js/assignments.js
+++ js/assignments.js
@@ -1,51 +1,32 @@
-
-$(document).ready(function() {
-  var myAssignments = new Firebase('https://dibbleapp.firebaseio.com/assignments/');
-
-  $('#assignments').on('change', 'div.assignment', function(e) {
-    var assignmentId = $(this).attr('id');
-    var assignmentRef = myAssignments.child(assignmentId);
-    var assignmentFile = $('#fileUpload')[0].files[0];
-
-    var reader = new FileReader();
-    // fr.onload = receivedText;
-    //fr.readAsText(file);
-    console.log(reader.readAsDataURL(assignmentFile));
-
-    assignmentRef.update({
-      "file": assignmentFile
-    });
-
-    // myAssignments.push({
-    //   title: entryTitle,
-    //   file: entryFile,
-    //   notes: entryNotes
-    // });
-
-  });
-
-  myAssignments.on("child_added", function (snapshot) {
-    var firebaseValue = snapshot.val();
-    displayAssignment(firebaseValue.title, firebaseValue.teacher, firebaseValue.description, snapshot.key());
-  });
-
-  function displayAssignment(title, teacher, description, id) {
-    var dateObj = new Date();
-    var month = dateObj.getUTCMonth() + 1; //months from 1-12
-    var day = dateObj.getUTCDate();
-    var year = dateObj.getUTCFullYear();
-    newdate = year + "/" + month + "/" + day;
-    var $title = $('<h3>').text(title).addClass('title');
-    var $teacher = $('<h4>').text('assigned by ' + teacher).addClass('teacher');
-    var $date = $('<p>').text(newdate).addClass('date');
-    var $description = $('<p>').text(description).addClass('notes');
-    var $file = $('<input>').attr('type', 'file').attr('id', 'fileUpload').attr('enctype', "multipart/form-data").addClass('file');
-    var $assignment = $('<div>').addClass('assignment').attr('id', id).append($title).append($teacher).append($date).append($description).append($file).append('<br><br>');
-
-    $('#assignments').append($assignment);
-    $('#assignments')[0].scrollTop = $('#assignments')[0].scrollHeight;
-  };
-
-
-
-});
+// var userRef = ref.child('users');
+// console.log('current user is ' + currentUser.uid);
+// var assignmentsRef = ref.child('users').child('ba_at_ba_dot_com').child('assignments');
+//
+// //
+// $('#assignments').on('change', 'input.file', function(e) {
+//   var assignmentId = $(this).parent('.assignment').attr('id');
+//   var uniqueAssigmentRef = assignmentsRef.child(assignmentId);
+//   var assignmentFile = ($(this))[0].files[0];
+//   var assignmentFilePreview = $(this).siblings('.filepreview');
+//
+//   var reader = new FileReader();
+//
+//   reader.onload = function(){
+//     var filePayload = reader.result;
+//     assignmentFile = assignmentRef
+//     assignmentFilePreview.attr('src', dataURL);
+//   };
+//
+//   reader.readAsDataURL(assignmentFile);
+//
+//   uniqueAssigmentRef.update({
+//     "file": "hi"
+//   });
+//
+//   // myAssignments.push({
+//   //   title: entryTitle,
+//   //   file: entryFile,
+//   //   notes: entryNotes
+//   // });
+//
+// });
--- js/global.js
+++ js/global.js
@@ -1,5 +1,219 @@
-$(document).ready(function() {
+//Global Variables
+var ref = new Firebase('https://dibbleapp.firebaseio.com/');
+var currentUser = {};
+var currentAssignments = {};
+var currentGroup;
+var assignmentsRef = ref.child('assignments');
+var usersRef = ref.child('users');
+var groupsRef = ref.child('groups');
+$('#header').load("_header.html");
+$('#header').on('click', '.logout', function(){
+  ref.unauth();
+  location.href = "login.html";
+});
 
-  $('#header').load("../views/_header.html");
+// USER AUTHENTICATION ------------------------------------------
+
+//Sign Up With Email
+$('#userSignUp').on('click', function(){
+  var usersRef = ref.child('users');
+  var email = $('#email').val();
+  var password = $('#password').val();
+
+  ref.createUser({
+    email    : email,
+    password : password
+  }, function(error, userData) {
+    if (error) {
+      console.log("Error creating user:", error);
+    } else {
+      console.log("Successfully created user account with uid:", userData.uid);
+    }
+  });
 
 });
+
+// Log In
+$('#userLogin').on('click', function(){
+  var email = $('#email').val();
+  var password = $('#password').val();
+
+  ref.authWithPassword({
+    "email"    : email,
+    "password" : password,
+  }, function(error, authData) {
+    if (error) {
+      console.log("Login Failed!", error);
+    } else {
+      console.log("Authenticated successfully with payload:", authData);
+      location.href = "index.html";
+    }
+  });
+});
+
+// Create a callback which logs the current auth state
+function authDataCallback(authData) {
+  if (authData) {
+    console.log("User " + authData.uid + " is logged in with " + authData.provider);
+    currentUser = authData;
+  } else {
+    console.log("User is logged out");
+    currentUser = {};
+  }
+}
+// Register the callback to be fired every time auth state changes
+ref.onAuth(authDataCallback);
+
+//Store User Data
+var isNewUser = true;
+ref.onAuth(function(authData) {
+  if (authData && isNewUser) {
+    // save the user's profile into the database so we can list users,
+    // use them in Security and Firebase Rules, and show profiles
+    ref.child("users").child(authData.uid).update({
+      provider: authData.provider,
+      name: getName(authData)
+    });
+  }
+});
+// find a suitable name based on the meta info given by each provider
+function getName(authData) {
+  switch(authData.provider) {
+     case 'password':
+       return authData.password.email.replace(/@.*/, '');
+  }
+};
+
+// DISPLAY USER'S GROUPS + SET CURRENTGROUP ------------------------------------------
+var usersGroupsRef = usersRef.child(currentUser.uid).child('groups');
+
+usersGroupsRef.on("value", function(snapshot) {
+  snapshot.forEach(function(childSnapshot) {
+    var group = childSnapshot.key();
+    displayGroups(group);
+  });
+});
+
+function displayGroups (group){
+  var $group = $('<a>').text(group).addClass('.group').val(group);
+  $('#groups').append($group).append('<br>');
+  currentGroup = group;
+};
+
+$('#groups').on("click", "a", function(){
+  currentGroup = $(this).val();
+  console.log('currentGroup is now ' + currentGroup);
+  loadAssignments();
+});
+
+
+// DISPLAY CURRENT GROUP'S ASSIGNMENTS -------------------------------------
+loadAssignments();
+function loadAssignments(){
+  $('#assignments').children('div').remove();
+
+  assignmentsRef.on("value", function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+      var assignment = childSnapshot.val();
+      var group = childSnapshot.val().group;
+      //isolate assignments available in current group ONLY
+      if (group == currentGroup){
+        //get comments for specified assignment
+        var commentsRef = assignmentsRef.child(childSnapshot.key()).child('comments');
+        var comments = [];
+        commentsRef.on("value", function(commentSnapshot) {
+          //iterate over each comment
+          commentSnapshot.forEach(function(commentChildSnapshot) {
+            var comment = commentChildSnapshot.val();
+            comments.push(comment);
+          });
+        });
+        displayAssignment(childSnapshot.key(), assignment.title, assignment.description, assignment.complete, comments);
+      };
+    });
+  });
+
+  function displayAssignment (key, title, description, complete, comments) {
+    var status;
+    var statusOpposite;
+    complete == true ? status = 'complete' : status = 'incomplete';
+    complete == true ? statusOpposite = 'incomplete' : statusOpposite = 'complete';
+    var $title = $('<h3>').text(title).addClass(status);
+    var $description = $('<p>').text(description).addClass(status);
+    var $buttonComplete = $('<button>').text('MARK AS ' + statusOpposite).addClass('button' + statusOpposite);
+    var $buttonDelete = $('<button>').text('DELETE ASSIGNMENT').addClass('buttonDelete');
+
+    //render comments
+    var $comments = $('<div>').addClass('comments').html('<h6>COMMENTS & FEEDBACK</h6>');
+    for (var i = 0; i < comments.length; i++){
+      var $comment = $('<p>').addClass('comment').html(comments[i].comment + '<br><span class="commentauthor">added by ' + comments[i].user + '</span>');
+      console.log(comments[i].comment);
+      $comments.append($comment);
+    };
+    var $commentFormInput = $('<input>').attr('type', 'text').attr('placeholder','Say something...').attr('id', 'commentFormInput');
+    var $commentFormSubmit = $('<button>').text('ADD').attr('id', 'commentFormSubmit');
+    var $commentForm = $('<div>').addClass('commentForm').append($commentFormInput).append($commentFormSubmit);
+    $comments = $comments.append($commentForm);
+
+    //put it all together now!
+    var $assignment = $('<div>').addClass('assignment').attr('id', key).addClass(status).append($title).append($description).append($comments).append('<br>').append($buttonComplete).append($buttonDelete);
+    $('#assignments').append($assignment);
+  };
+};
+
+// mark assignment as complete
+$('#assignments').on('click', '.buttoncomplete', function(){
+  var assignmentKey = $(this).parent('.assignment').attr('id');
+  assignmentsRef.child(assignmentKey).update({
+    complete : true
+  });
+  loadAssignments();
+});
+
+$('#assignments').on('click', '.buttonincomplete', function(){
+  var assignmentKey = $(this).parent('.assignment').attr('id');
+  assignmentsRef.child(assignmentKey).update({
+    complete : false
+  });
+  loadAssignments();
+});
+
+// delete assignment
+$('#assignments').on('click', '.buttonDelete', function(){
+  var assignmentKey = $(this).parent('.assignment').attr('id');
+  assignmentsRef.child(assignmentKey).remove();
+  loadAssignments();
+});
+
+// add assignment
+$('.addAssignment').on('click', function(){
+  $('#assignmentForm').toggle();
+});
+
+$('#assignmentFormSubmit').on('click', function(){
+  assignmentsRef.push({
+    title: $('#assignmentTitleInput').val(),
+    description: $('#assignmentDescriptionInput').val(),
+    group: currentGroup,
+    complete: false
+  });
+  $('#assignmentTitleInput').val('');
+  $('#assignmentDescriptionInput').val('');
+  $('#assignmentForm').hide();
+  loadAssignments();
+});
+
+// add comment
+$('#assignments').on('click', '#commentFormSubmit', function(){
+  var assignmentKey = $(this).parent().parent().parent('.assignment').attr('id');
+  console.log($(this).attr('id'));
+  var commentsRef = assignmentsRef.child(assignmentKey).child('comments');
+  commentsRef.push({
+    comment: $('#commentFormInput').val(),
+    user: currentUser.uid
+  });
+  $('#commentFormInput').val('');
+  loadAssignments();
+});
+
+//delete comment
